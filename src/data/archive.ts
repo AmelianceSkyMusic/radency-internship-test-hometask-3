70add2315fc1468406d5864a4cd421706da50036
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-07 17:29:27 +0300

    * add init data

---
+++ src/data/archive.ts
@@ -0,0 +1,25 @@
+import { Note } from 'src/types/Note';
+
+export const archive: Note[] = [
+	{
+		id: 1640597959446,
+		created: 1640597959446,
+		name: 'Fitness',
+		category: 'task',
+		content: 'Start a new fitness routine to stay active and healthy',
+	},
+	{
+		id: 1620597959446,
+		name: 'Sunday Todo',
+		created: 1620597959446,
+		category: 'task',
+		content: 'Finish the report, Buy groceries, Exercise for 30 minutes',
+	},
+	{
+		id: 1430597959446,
+		name: 'Create a blog',
+		created: 1430597959446,
+		category: 'idea',
+		content: 'Create a blog to share your travel experiences and tips',
+	},
+];
---
+++ src/data/notes.ts
@@ -0,0 +1,55 @@
+import { Note } from 'src/types/Note';
+
+export const notes: Note[] = [
+	{
+		id: 1690997959446,
+		name: 'Shopping list',
+		created: 1690997959446,
+		category: 'task',
+		content: '1. Milk, 2. Bread, 3. Apples, 4. Pasta',
+	},
+	{
+		id: 1690597959446,
+		name: 'Сaring',
+		created: 1690597959446,
+		category: 'thought',
+		content: 'Is it possible to get a job without knowing caring?',
+	},
+	{
+		id: 1690237959446,
+		name: 'Obsidian',
+		created: 1690237959446,
+		category: 'quote',
+		content: "Obsidian's versatile and interconnected notes",
+	},
+	{
+		id: 1690177959446,
+		name: 'To Learn',
+		created: 1690177959446,
+		category: 'task',
+		content: 'GraphQL, Apollo, T3 App',
+	},
+	{
+		id: 1690057959446,
+		name: 'Revolutionary React',
+		created: 1690057959446,
+		category: 'idea',
+		content:
+			"React's component-based architecture empowers developers to build dynamic, interactive user interfaces effortlessly",
+	},
+	{
+		id: 1690447959446,
+		name: 'Radency internship',
+		created: 1690447959446,
+		category: 'task',
+		content: 'Take an internship at Radency',
+	},
+	{
+		id: 1689767959446,
+		name: 'Dentist',
+		created: 1689767959446,
+		category: 'task',
+		content:
+			'I’m gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021',
+	},
+];
